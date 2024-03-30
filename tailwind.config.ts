d9255cfd7f498600402eb8194914cc6afae0be30
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2024-03-29 22:53:23 -0300

    style: add tokens in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,24 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    darkMode: 'class',
+    theme: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        red: '#DD3D3D',
+        gray: {
+          400: '3f3f3f',
+          500: '#2f2f2f',
+          700: '#1f1f1f',
+        },
+        white: {
+          500: '#f2f2f2',
+          700: '#f0f0f0',
+        },
       },
     },
   },
